f37de1eeb9b4b2a7638191cd7608cdac6905192a
ryan-hayden <ryanhayden@live.com>
2023-04-12 19:56:58 -0400

    detected collisions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -34,9 +34,11 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
     }
 }
